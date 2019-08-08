826d41caa1ef5409fb7d526f4a14f9d5172db2bf
Such <360996299@qq.com>
2019-08-08 23:10:11 +0800

    config.py

---
+++ www/config_default.py
@@ -0,0 +1,24 @@
+#！/user/bin/env python3
+# -*- coding:utf-8 -*-
+
+__author__ = 'Scuh'
+
+'''
+Default configgurations.
+'''
+
+configs = {
+	'debug':True,
+	'db':{
+	'host':'127.0.0.1',
+	'port':3306,
+	'user':'Such',
+	'password':'1234567',
+	'db':'awesome'
+	},
+	'session':{
+	'secret':'Awesome'
+	}
+}
+
+print(configs.items())
