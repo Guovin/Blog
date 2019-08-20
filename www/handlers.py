2b98fea094e891b2d3c48caa70574a252ad7bdf5
Such <360996299@qq.com>
2019-08-20 23:45:30 +0800

    manage_blogs

--- www/handlers.py
+++ www/handlers.py
@@ -12,7 +12,9 @@ import re,time,json,logging,hashlib,base64,asyncio
 from aiohttp import web
 
 from coroweb import get,post
-from apis import APIValueError,APIResourceNotFoundError,APIError
+from apis import APIValueError,APIResourceNotFoundError,APIError,APIPermissionError
+
+from apis import Page
 
 from models import User,Comment,Blog,next_id
 from config import configs
@@ -142,3 +144,60 @@ def signout(request):
 	logging.info('user signed out.')
 	return r
 
+@get('/manage/blogs/create')
+def manage_create_blog():
+	return{
+		'__template__': 'manage_blog_edit.html',
+		'id': '',
+		'action': '/api/blogs'
+	}
+
+
+def check_admin(request):
+	if request.__user__ is None or not request.__user__.admin:
+		raise APIPermissionError()
+
+@post('/api/blogs')
+async def api_create_blog(request,*,name,summary,content):
+	check_admin(request)
+	if not name or not name.strip():
+		raise APIValueError('name','name cannot be empty.')
+	if not summary or not summary.strip():
+		raise APIValueError('summary','summary cannot be empty.')
+	if not content or not content.strip():
+		raise APIValueError('content','content cannot be empty.')
+	blog = Blog(user_id=request.__user__.id,user_name=request.__user__.name,user_image=request.__user__.image,name=name.strip(),summary=summary.strip(),content=content.strip())
+	await blog.save()
+	return blog
+
+def get_page_index(page_str):
+	p = 1
+	try:
+		p = int(page_str)
+	except ValueError as e:
+		pass
+	if p < 1:
+		p = 1
+	return p
+
+@get('/api/blogs')
+async def api_blogs(*,page='1'):
+	page_index = get_page_index(page)
+	num = await Blog.findNumber('count(id)')
+	p = Page(num,page_index)
+	if num == 0 :
+		return dict(page=p,blogs=())
+	blogs = await Blog.findAll(orderBy='created_at desc',limit=(p.offset,p.limit))
+	return dict(page=p,blogs=blogs)
+
+@get('/api/blogs/{id}')
+async def api_get_blog(*,id):
+	blog = await Blog.find(id)
+	return blog
+
+@get('/manage/blogs')
+def manage_blogs(*,page='1'):
+	return {
+		'__template__':'manage_blogs.html',
+		'page_index':get_page_index(page)
+	}
